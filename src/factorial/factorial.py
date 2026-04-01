b32bec37011858426ef4954616735b5fe41832a7
Nahuel Berthet <nahuelberthet3@gmail.com>
2026-03-31 21:13:36 -0300

    Se agrego funcion para calcular rango de factorial

--- src/factorial/factorial.py
+++ src/factorial/factorial.py
@@ -5,32 +5,56 @@
 #* Dr.P.E.Colla (c) 2022                                                   *
 #* Creative commons                                                        *
 #*-------------------------------------------------------------------------*
+
+
+
 import sys
+
 def factorial(num): 
     if num < 0: 
         print("Factorial de un número negativo no existe")
-        return 0
+        return None
     elif num == 0: 
         return 1
-        
     else: 
         fact = 1
-        while(num > 1): 
+        while num > 1: 
             fact *= num 
             num -= 1
         return fact 
 
-#Realice una modificación al programa para que si se omite el número como argumento lo solicite
-if len(sys.argv) < 2:
-    num = int(input("Ingrese un número: "))
-else:
-    num = int(sys.argv[1])
+def obtener_rango(entrada):
+    try:
+        partes = entrada.split("-")
+        if len(partes) == 2:
+            inicio = int(partes[0])
+            fin = int(partes[1])
+            return inicio, fin
+        else:
+            # Si solo se pasa un numero
+            num = int(entrada)
+            return num, num
+    except ValueError:
+        print("Entrada invalida. Debe ser un numero o un rango desde-hasta (ej. 4-8).")
+        sys.exit()
 
-print("Factorial ",num,"! es ", factorial(num)) 
+# Pedir argumento 
+if len(sys.argv) < 2:
+    entrada = input("Ingrese un numero o un rango (ej. 4-8): ")
+else:
+    entrada = sys.argv[1]
+
+inicio, fin = obtener_rango(entrada)
+
+# Calcular factoriales en el rango
+for n in range(inicio, fin + 1):
+    resultado = factorial(n)
+    if resultado is not None:
+        print(f"Factorial {n}! es {resultado}")
+
 
 #* codigo viejo
 # if len(sys.argv) < 0: 
 #    print("Debe informar un número!")
 #    sys.exit()
 # num=int(sys.argv[1])
-
